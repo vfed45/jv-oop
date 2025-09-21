61e287c961bee5ec33c20ec6fbb7ee1c409ee008
Viktoriia <fvee737@gmail.com>
2025-09-21 10:39:49 +0200

    fixed bugs

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,8 +1,9 @@
 package core.basesyntax;
 
 public class MainApp {
+
     public static void main(String[] args) {
-        Machine[] machines =  new Machine[3];
+        Machine[] machines = new Machine[3];
 
         machines[0] = new Truck();
         machines[1] = new Bulldozer();
