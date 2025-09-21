7f614ff4480129a7a9653375fd3d3224818e2b7a
Viktoriia <fvee737@gmail.com>
2025-09-21 10:10:02 +0200

    Implement the full Machine hierarchy and MainApp

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+public class Excavator extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("core.basesyntax.Excavator started its work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("core.basesyntax.Excavator stopped working");
+    }
+}
